d02810c857c89d5169efcb34dddab8790960f8e2
JBinkowski <jan.binkowski96@gmail.com>
2024-07-18 21:55:48 +0000

    Updated

--- info.sh
+++ info.sh
@@ -1,4 +1,4 @@
-echo Running container using following dependencies: 
+echo Installed tools:
 
 echo Python: $(python3.10 --version)
 echo R: $(R --version)
@@ -7,3 +7,9 @@ echo tabix: $(tabix --version)
 echo BCFtools: $(bcftools --version)
 echo Illumina CLI: $(array-analysis-cli --version)
 echo VEP: $(vep -v)
+
+echo R environment:
+echo $(Rscript -e 'sesame::sesame_checkVersion()')
+
+echo Python environment:
+echo $(python3.10 -m pip freeze)
